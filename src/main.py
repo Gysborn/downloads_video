d39ffbc852445effd10e4960683b4276922cf57b
Gysborn <forparty@gmail.com>
2024-08-31 19:53:26 +0300

    new commit

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ def download(link, name='%(title)s'):
     ydl_opts = {
         # 'format': 'bestvideo+bestaudio/best', #берем самое лучшее качество видео и фото
         'outtmpl': '{}.%(ext)s'.format(name), #наше выбраное имя, если его не было, то стандартное - название видео на самом сайте
-        'login': 'i9046495703@gmail.com',
+        'username': 'i9046495703@gmail.com',
         'password':'Ex7-9bf-ash-XMk',
     }
     with st.spinner('Загрузка...'):
